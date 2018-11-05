857aa74bffdb4fc16dd354598e915d1a2d8397ec
Thomas De Praetere <thomas@dokeos.com>
2018-11-05 16:25:24 +0100

    Dokeos 4.0 root

---
+++ session.php
@@ -0,0 +1,10 @@
+<?php
+/* For licensing terms, see /license.txt */
+
+// including the global Dokeos file
+require ('main/inc/global.inc.php');
+global $_configuration;
+echo '------------';
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
